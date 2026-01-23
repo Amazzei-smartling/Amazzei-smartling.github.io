70429e7b90b2946fafdd2f97e8fd060d93ee45bf
Alessio Mazzei <alessiomazzei@Host-005.home>
2026-01-23 11:56:25 +0000

    test

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function sl_tr_json_start(){}
-function sl_tr_json_end(){}
+function sl_translate() {}
+sl_tr_json_start = sl_translate;
+sl_tr_json_end = sl_translate;
 
-function func(){
-    sl_tr_json_start();        
+function testFunc(){
+    sl_tr_json_start();
     return {
         "sl_translate": "sl_all",
-        "sl_variant": "key1: variantForWhite, key2: variantForPurple",
-        "key1": "white",
-        "key2": "purple"            
+        "sl_variant": "key1: variantForMagenta, key2: variantForBrown",
+        "key1": "magenta",
+        "key2": "brown"            
     }
     sl_tr_json_end();
 }    
