afa6944c926b79fa461aac53c083e68d43ed21a1
Alessio Mazzei <alessiomazzei@Host-005.home>
2026-01-23 11:30:29 +0000

    test

---
+++ script.js
@@ -0,0 +1,13 @@
+function sl_tr_json_start(){}
+function sl_tr_json_end(){}
+
+function func(){
+    sl_tr_json_start();        
+    return {
+        "sl_translate": "sl_all",
+        "sl_variant": "key1: variantForWhite, key2: variantForPurple",
+        "key1": "white",
+        "key2": "purple"            
+    }
+    sl_tr_json_end();
+}    
