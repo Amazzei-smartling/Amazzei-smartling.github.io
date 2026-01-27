c2ce45edfd8992b80571912d1f25fc7470df3f8e
Alessio Mazzei <alessiomazzei@Mac.home>
2026-01-27 08:49:44 +0000

    test

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function testFunc(){
         /* <sl:translate_json> */ 
         "sl_translate": "sl_all",
         "sl_variant": "key1: variantForMouse, key2: variantForGoat",
-        key1: "mouse",
+        "key1": "mouse",
         "key2": "goat"        
         /* </sl:translate_json> */
     }    
