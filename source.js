8a0d7c11b301f21f6734e2a6a7239d77fdae6782
Alessio Mazzei <amazzei@smartling.com>
2026-06-03 15:39:37 +0100

    test

--- source.js
+++ source.js
@@ -1,7 +1,7 @@
 function stringVariants(e){return({/*! <sl:translate_json> */ "sl_translate":"sl_all","sl_variant":"get-started-colloquial:get-started-colloquial,start-a-free-trial-colloquial:start-a-free-trial-colloquial","get-started-colloquial":"Get started","start-a-free-trial-colloquial":"\n                  Start a Free Trial\n                "})[e];/*! </sl:translate_json> */}
 
 function $f(e) {
-    /* <sl:translate_json> *//*[ JSON PARSING BEGIN ]*/
+    /* <sl:translate_json> */
     return {
         "sl_translate": "sl_all",
         "sl_variant": "sl-4e4814595720fdc456162ea666828e34:4e4814595720fdc456162ea666828e34,sl-69f9faa8b37f78d9a3c41020dc15238c:69f9faa8b37f78d9a3c41020dc15238c",
